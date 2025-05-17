2add37c6398b53007e3eee35c2a500f62586629a
tahyphen <97334784+tahyphen@users.noreply.github.com>
2025-05-17 22:33:57 +0900

    Add config for vyfor/cord.nvim

---
+++ neovim/after/pack/vyfor/start/cord.nvim/plugin/setup.lua
@@ -0,0 +1,18 @@
+require('cord').setup {
+  text = {
+    workspace = 'In workspace',
+    viewing = 'Viewing a file',
+    editing = 'Editing a file',
+    file_browser = 'Browsing files',
+    plugin_manager = 'Managing plugins',
+    lsp = 'Configuring LSP',
+    docs = 'Reading docs',
+    vcs = 'Committing changes',
+    notes = 'Taking notes',
+    debug = 'Debugging',
+    test = 'Testing',
+    diagnostics = 'Fixing problems',
+    games = 'Playing a game',
+    terminal = 'Running commands',
+  },
+}
